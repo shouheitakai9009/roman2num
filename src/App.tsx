c78f1c9aa7a0193ab0d5cf59ca7e8d47e9f95ff4
shouheitakai9009 <shouheitakai.me@gmail.com>
2024-01-26 22:57:50 +0900

    Add the description in header

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 import { TextInput } from "./components/TextInput";
 import { useValidate } from "./hooks/useValidate";
 import { romanToNumber } from "./utils/romanToNumber";
+import { Header } from "./components/Header";
 
 function App() {
   const [roman, setRoman] = useState<string>("");
   const errorMessage = useValidate({ roman });
 
   return (
-    <article className="h-full flex justify-center items-center">
-      <main className="w-[1080px] sm:w-full px-4 flex flex-col justify-center items-center">
-        <img src="/icon.webp" className="w-56" />
-        <h1 className="text-3xl md:text-3xl font-mono tracking-tighter my-2">
-          Roman To Number
-        </h1>
+    <article className="h-full overflow-hidden flex flex-col items-center">
+      <Header />
+      <main className="w-[90%] md:w-[1080px] h-full px-4 flex flex-col justify-center items-center">
         <section className="flex items-center gap-4 md:flex-row flex-col mt-4">
           <TextInput
             value={roman}
@@ -27,6 +25,7 @@ function App() {
             readOnly
             className="w-32"
           />
+          <img src="/icon.webp" className="w-36 md:w-56" />
         </section>
       </main>
     </article>
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -17,10 +17,10 @@ export const TextInput: React.FC<Props> = ({
         className={cn(
           "h-14 w-full rounded-lg px-4",
           "focus:ring-offset-2 focus:ring-2 outline-none",
-          "text-3xl font-bold text-cyan-800",
+          "text-3xl font-bold shadow-md",
           errorMessage
-            ? "border-red-500 border focus:ring-red-500"
-            : "border-cyan-600 border focus:ring-cyan-600"
+            ? "text-red-500 border-red-500 shadow-red-100 border focus:ring-red-500"
+            : "text-sky-700 border-sky-700 shadow-sky-100 border focus:ring-sky-700"
         )}
         {...props}
       />
