2b8dccae9e2afb5139f3815c8771acb13ae4525e
shouheitakai9009 <shouheitakai.me@gmail.com>
2024-01-27 21:06:27 +0900

    Adjust layouts

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   const errorMessage = useValidate({ roman });
 
   return (
-    <article className="h-full overflow-hidden flex flex-col items-center">
-      <Header />
-      <main className="w-[90%] md:w-[1080px] h-full px-4 flex flex-col justify-center items-center">
+    <article className="h-full overflow-hidden flex justify-center items-center">
+      <main className="w-[90%] md:w-[720px] h-full px-4 flex flex-col justify-between md:justify-center gap-6 items-center">
+        <Header />
         <section className="flex items-center gap-4 md:flex-row flex-col mt-4">
           <TextInput
             value={roman}
@@ -25,8 +25,21 @@ function App() {
             readOnly
             className="w-32"
           />
-          <img src="/icon.webp" className="w-36 md:w-56" />
         </section>
+        <p className="text-md tracking-wider mb-6">
+          The app allows you to convert Roman to a number.
+          <br />
+          For example, the output is "6" when you input "VI" because V=5 and
+          I=1.
+          <br />
+          When A left character is less than a right character, two numbers are
+          added, when a right character is less than a left character, two
+          numbers are subtracted.
+          <br />
+          All Roman characters are I=1, V=5, X=10, L=50, C=100, D=500, and
+          M=1000.
+          <br />
+        </p>
       </main>
     </article>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,10 @@
 export const Header = () => {
   return (
     <header className="w-full flex justify-center mt-6">
-      <div className="w-[90%] md:w-[500px]">
-        <h1 className="text-xl md:text-2xl font-mono text-sky-700 flex items-center justify-center">
+      <div className="w-[90%] md:w-[720px]">
+        <h1 className="text-2xl md:text-5xl font-mono text-sky-700 flex items-center justify-center">
           Roman To Number
         </h1>
-        <p className="text-xs mt-4">
-          The app allows you to convert Roman to a number.
-          <br />
-          For example, the output is "6" when you input "VI" because V=5 and
-          I=1.
-          <br />
-          When A left character is less than a right character, two numbers are
-          added, when a right character is less than a left character, two
-          numbers are subtracted.
-          <br />
-          All Roman characters are I=1, V=5, X=10, L=50, C=100, D=500, and
-          M=1000.
-          <br />
-        </p>
       </div>
     </header>
   );
